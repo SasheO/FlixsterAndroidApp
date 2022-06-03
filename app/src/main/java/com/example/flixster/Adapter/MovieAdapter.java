e491352441f52a03cd637f11b4306f2862dbd9bc
Mezisashe Ojuba <sasheojuba@fb.com>
2022-06-03 11:12:01 -0700

    first commit: app is functional displaying list of movies gotten from API, changing orientation from landscape to portrait with a few bugs

---
+++ app/src/main/java/com/example/flixster/Adapter/MovieAdapter.java
@@ -0,0 +1,80 @@
+package com.example.flixster.Adapter;
+
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+import java.util.List;
+import android.content.Context;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies){
+        this.movies = movies;
+        this.context = context;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false );
+        return new ViewHolder(movieView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // get the movie at the passed in position
+        // bind the movie data into the view hold
+        Movie movie = movies.get(position);
+         holder.bind(movie); // bind function is defined later in this class
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                Glide.with(context).load(movie.getBackdropPath()).placeholder(R.drawable.flicks_backdrop_placeholder)
+                        .error(R.drawable.flicks_backdrop_placeholder).into(ivPoster);
+
+            }
+            else {
+                Glide.with(context).load(movie.getPosterPath()).placeholder(R.drawable.flicks_movie_placeholder)
+                        .error(R.drawable.flicks_movie_placeholder).into(ivPoster);
+
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.Adapter.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import android.os.Bundle;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    List<Movie> movies;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies );
+        movies = new ArrayList<>();
+
+        // create an adapter
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        // set the adapter on recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        // set a Layout manager on the adapter view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this ));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                 Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged(); // you must notify the adapter any time that the data set an adapter refers to is modified. Initially, the movies set was empty but now, it is populated with data from the API.
+                    Log.d(TAG, "movies: " + movies.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "Hit json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+
+            }
+        });
+    }
+}
